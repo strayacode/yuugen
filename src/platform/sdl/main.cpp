878cdc3fd926f3dda6d43f0f6cc8e0456ab2ecaa
strayacode <dallascuber@gmail.com>
2020-12-16 20:56:28 +1100

    fix bug where emulator segfaults when no arguments are specified

--- src/platform/sdl/main.cpp
+++ src/platform/sdl/main.cpp
@@ -1,11 +1,12 @@
 #include <emulator/emulator.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     Emulator emulator;
     if (argc < 2) {
         printf("no rom argument or other arguments were specified!\n");
-        return 0;
+        exit(1);
     }
     int i;
     for (i = 1; i < argc - 1; i++) {
