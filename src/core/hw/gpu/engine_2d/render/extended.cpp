e50bf5fba1d0bc590df0b5e827c7e86d0a6c9573
strayacode <dallascuber@gmail.com>
2021-10-15 12:00:35 +1100

    implement extended 16 bit bg entries

--- src/core/hw/gpu/engine_2d/render/extended.cpp
+++ src/core/hw/gpu/engine_2d/render/extended.cpp
@@ -28,9 +28,41 @@ void GPU2D::RenderExtended(int bg_index, u16 line) {
         break;
     }
 
+    // if (BGCNT[bg_index] & (1 << 13)) {
+    //     log_fatal("handle");
+    // }
+
     if (!(BGCNT[bg_index] & (1 << 7))) {
         // 16 bit bgmap entries
-        // log_warn("[GPU2D] Handle 16 bit bg map entries");
+        u32 character_base = vram_addr + (((BGCNT[bg_index] >> 2) & 0xF) * 0x4000) + (((DISPCNT >> 24) & 0x7) * 0x10000);
+        u32 screen_base = vram_addr + (((BGCNT[bg_index] >> 8) & 0x1F) * 0x800) + (((DISPCNT >> 27) & 0x7) * 0x10000);
+        u16 size = 128 << screen_size;
+        for (int pixel = 0; pixel < 256; pixel++) {
+            // get rotscal coords
+            u32 coord_x = (internal_x[bg_index - 2] + BGPA[bg_index - 2] * pixel) >> 8;
+            u32 coord_y = (internal_y[bg_index - 2] + BGPC[bg_index - 2] * pixel) >> 8;
+
+            // don't draw the pixel if the x and y coordinates are not inside the dimensions of the tilemap
+            if (coord_x < 0 || coord_x >= size || coord_y < 0 || coord_y >= size) {
+                continue;
+            }
+
+            // get the tile address
+            u32 screen_addr = screen_base + ((coord_y / 8) * (size / 8) + (coord_x / 8)) * 2;
+            u16 tile_info = gpu->ReadVRAM<u16>(screen_addr);
+
+            u32 tile_number = tile_info & 0x3FF;
+            u8 horizontal_flip = (tile_info >> 10) & 0x1;
+            u8 vertical_flip = (tile_info >> 11) & 0x1;
+
+            u32 character_addr = character_base + (tile_number * 64);
+            character_addr += (vertical_flip ? ((7 - coord_y % 8) * 8) : ((coord_y % 8) * 8));
+            character_addr += (horizontal_flip ? (7 - coord_x % 8) : (coord_x % 8));
+            
+            u8 palette_index = gpu->ReadVRAM<u8>(character_addr);
+            u16 colour = ReadPaletteRAM<u16>(palette_index * 2);
+            bg_layers[bg_index][(256 * line) + pixel] = colour;
+        }
     } else if ((BGCNT[bg_index] & (1 << 7)) && (BGCNT[bg_index] & (1 << 2))) {
         // direct colour bitmap
         for (int pixel = 0; pixel < 256; pixel++) {
