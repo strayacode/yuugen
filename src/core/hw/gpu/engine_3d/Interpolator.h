0bb39b035fcaca3a14a67865a684a84014482686
strayacode <dallascuber@gmail.com>
2022-04-09 15:27:11 +1000

    handle linear interpolation fallback

--- src/core/hw/gpu/engine_3d/Interpolator.h
+++ src/core/hw/gpu/engine_3d/Interpolator.h
@@ -9,6 +9,10 @@ class Interpolator {
 public:
     // we assume that x is between x0 and x1 and x0 < x1
     u32 interpolate(u32 u0, u32 u1, u32 x, u32 x0, u32 x1, u32 w0, u32 w1) {
+        if (use_linear_interpolation(w0, w1)) {
+            return interpolate_linear(u0, u1, x, x0, x1);
+        }
+
         u32 t0 = x - x0;
         u32 t1 = x1 - x;
         u32 denom = t0 * w0 + t1 * w1;
@@ -35,9 +39,9 @@ public:
 
     Colour interpolate_colour(Colour c1, Colour c2, u32 x, u32 x0, u32 x1, u32 w0, u32 w1) {
         Colour c3;
-        c3.r = interpolate_linear(c1.r, c2.r, x, x0, x1);
-        c3.g = interpolate_linear(c1.g, c2.g, x, x0, x1);
-        c3.b = interpolate_linear(c1.b, c2.b, x, x0, x1);
+        c3.r = interpolate(c1.r, c2.r, x, x0, x1, 8192, 8192);
+        c3.g = interpolate(c1.g, c2.g, x, x0, x1, 8192, 8192);
+        c3.b = interpolate(c1.b, c2.b, x, x0, x1, 8192, 8192);
 
         return c3;
     }
@@ -58,6 +62,14 @@ public:
             return (u0 + ((u1 - u0) * factor)) >> precision;
         } else {
             return (u1 + ((u0 - u1) * ((1 << precision) - factor))) >> precision;
+        }
+    }
+
+    bool use_linear_interpolation(u32 w0, u32 w1) {
+        if constexpr (precision == 9) {
+            return w0 == w1 && (w0 & 0x7E) == 0 && (w1 & 0x7E) == 0;
+        } else {
+            return w0 == w1 && (w0 & 0x7F) == 0 && (w1 & 0x7F) == 0;
         }
     }
 private:
