144b51ac7c7e458b3512d3152bcdbd20dc718b7e
strayacode <dallascuber@gmail.com>
2023-05-02 10:44:04 +1000

    start vram impl

---
+++ src/core/nds/video/vram.cpp
@@ -0,0 +1,30 @@
+#include "core/nds/video/vram.h"
+
+namespace core::nds {
+
+VRAM::reset() {
+    vramcnt.fill(0);
+    bank_a.fill(0);
+    bank_b.fill(0);
+    bank_c.fill(0);
+    bank_d.fill(0);
+    bank_e.fill(0);
+    bank_f.fill(0);
+    bank_g.fill(0);
+    bank_h.fill(0);
+    bank_i.fill(0);
+}
+
+void VRAM::write_vramcnt(Bank bank, u8 value) {
+    const u8 masks[] = {0x9b, 0x9b, 0x9f, 0x9f, 0x87, 0x9f, 0x9f, 0x83, 0x83};
+    int index = static_cast<int>(bank);
+    value &= masks[index];
+
+    if (vramcnt[index] == value) {
+        return;
+    }
+
+    vramcnt[index] = value;
+}
+
+} // namespace core::nds
--- src/core/nds/video/vram.h
+++ src/core/nds/video/vram.h
@@ -1,11 +1,40 @@
 #pragma once
 
+#include <array>
+#include "common/types.h"
+
 namespace core::nds {
 
 class VRAM {
 public:
+    void reset();
+
+    enum class Bank {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+    };
+
+    void write_vramcnt(Bank bank, u8 value);
 
 private:
+    std::array<u8, 9> vramcnt;
+
+    std::array<u8, 0x20000> bank_a;
+    std::array<u8, 0x20000> bank_b;
+    std::array<u8, 0x20000> bank_c;
+    std::array<u8, 0x20000> bank_d;
+    std::array<u8, 0x10000> bank_e;
+    std::array<u8, 0x4000> bank_f;
+    std::array<u8, 0x4000> bank_g;
+    std::array<u8, 0x8000> bank_h;
+    std::array<u8, 0x4000> bank_i;
 };
 
 } // namespace core::nds
