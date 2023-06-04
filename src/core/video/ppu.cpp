813e513f2e3fef9accff9deefaa343554d010616
strayacode <dallascuber@gmail.com>
2023-06-05 09:05:08 +1000

    minor style inconsistency

--- src/core/video/ppu.cpp
+++ src/core/video/ppu.cpp
@@ -112,9 +112,9 @@ void PPU::render_vram_display(int line) {
 }
 
 u32 PPU::rgb555_to_rgb888(u32 colour) {
-    u8 b = ((colour & 0x1F) * 255) / 31;
-    u8 g = (((colour >> 5) & 0x1F) * 255) / 31;
-    u8 r = (((colour >> 10) & 0x1F) * 255) / 31;
+    u8 b = ((colour & 0x1f) * 255) / 31;
+    u8 g = (((colour >> 5) & 0x1f) * 255) / 31;
+    u8 r = (((colour >> 10) & 0x1f) * 255) / 31;
     return (r << 16) | (g << 8) | b;
 }
 
