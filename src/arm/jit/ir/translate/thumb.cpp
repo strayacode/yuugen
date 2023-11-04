d85bb931a1babfbd0378323346839aaf090d8c43
strayacode <dallascuber@gmail.com>
2023-11-04 16:03:57 +1100

    translate thumb_software_interrupt

--- src/arm/jit/ir/translate/thumb.cpp
+++ src/arm/jit/ir/translate/thumb.cpp
@@ -463,8 +463,21 @@ Translator::BlockStatus Translator::thumb_shift_immediate() {
 }
 
 Translator::BlockStatus Translator::thumb_software_interrupt() {
-    logger.todo("Translator: handle thumb_software_interrupt");
-    return BlockStatus::Continue;
+    // TODO: make sure the exception base when compiling doesn't change later
+    auto exception_address = jit.coprocessor.get_exception_base() + 0x08;
+    auto instruction_size = ir.basic_block.location.get_instruction_size();
+    auto cpsr = ir.load_cpsr();
+    ir.store_spsr(cpsr, Mode::SVC);
+
+    auto cpsr_cleared = ir.bitwise_and(cpsr, ir.constant(~0x1f));
+    auto cpsr_new = ir.bitwise_or(cpsr_cleared, ir.constant(static_cast<u32>(Mode::SVC)));
+    ir.store_cpsr(cpsr_new);
+    ir.store_flag(Flag::T, ir.constant(false));
+    ir.store_flag(Flag::I, ir.constant(true));
+    
+    ir.store_gpr(GPR::LR, Mode::SVC, ir.constant(ir.basic_block.current_address + instruction_size));
+    ir.store_gpr(GPR::PC, Mode::SVC, ir.constant(exception_address + (4 * instruction_size)));
+    return BlockStatus::Break;
 }
 
 Translator::BlockStatus Translator::thumb_branch_conditional() {
